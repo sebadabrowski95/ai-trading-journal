28d266604b3c56d370a42d16efa73b0878fe0518
sebadabrowski95 <sebadabrowski95@gmail.com>
2026-02-22 22:03:16 +0100

    init project

---
+++ src/main/java/com/example/aitradingjournalbackend/AiTradingJournalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.aitradingjournalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTradingJournalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTradingJournalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aitradingjournalbackend/AiTradingJournalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitradingjournalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTradingJournalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
